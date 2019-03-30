651e747771decae9226a286a0239fcf5a633eead
Foo@Win <jf_mejiar547@live.com>
2019-03-29 22:32:48 -0600

    initial commit, set querys and start project

--- SistemaHotel/Views/mainFrm.cs
+++ SistemaHotel/Views/mainFrm.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SistemaHotel
+namespace SistemaHotel.Views
 {
-    public partial class Form1 : Form
+    public partial class mainFrm : Form
     {
-        public Form1()
+        public mainFrm()
         {
             InitializeComponent();
         }
