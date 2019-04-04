201d2205587c314a9ccdd7a057f4acd173eac406
sauliño <saulantonio0317@gmail.com>
2019-04-04 09:57:32 -0600

    reserva y reservahabitacion 4

--- SistemaHotel/Control/CReserva.cs
+++ SistemaHotel/Control/CReserva.cs
@@ -44,6 +44,19 @@ namespace SistemaHotel.Control
             
         }
 
+        public void insertar_datos(int idcliente, int idempleado, DateTime fechareserva,
+            string formapago, string divisa, string stat)
+        {
+            MReserva obj = new MReserva();
+            obj.Id_cliente = idcliente;
+            obj.Id_empleado = idempleado;
+            obj.Fecha_reserva = fechareserva;
+            obj.Forma_pago = formapago;
+            obj.Divisa = divisa;
+            obj.Stat = stat;
+            obj.Insertar(obj);
+
+        }
 
     }
 }
