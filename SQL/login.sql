52b70df9dc5e905bc5890e2a9683c741e5c44c7b
Foo@Win <jf_mejiar547@live.com>
2019-04-04 22:24:44 -0600

    login sql

--- SQL/login.sql
+++ SQL/login.sql
@@ -16,6 +16,21 @@ AS
 
 EXEC sp_in_sysuser 'Admin', 'Admin', 'SysAdmin';
 EXEC sp_in_sysuser 'User1', 'Abc.123', 'FullUser';
-EXEC sp_in_sysuser 'User2', 'Def.456', 'Salesman';
+EXEC sp_in_sysuser 'User2', 'Def.456', 'Reserver';
 EXEC sp_in_sysuser 'User3', 'Ghi.789', 'Reserver';
 
+select * from sysuser;
+
+CREATE PROCEDURE log_in
+@usrname VARCHAR(25), @pswd VARCHAR(100)
+AS
+	if exists(SELECT * FROM sysuser WHERE usrname=@usrname AND DECRYPTBYPASSPHRASE(@pswd, pswd)=@pswd)
+	begin
+		SELECT 'GRANTED'
+	end
+	else
+	begin
+		SELECT 'DENIED'
+	end
+
+exec log_in 'Admin', 'Admin';
